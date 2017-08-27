066b355ffc2593639aa98c3bdb927c98718683d2
mayukha <scam2050@gmail.com>
2017-08-26 19:14:28 -0700

    working version of ble communication

--- advert.js
+++ advert.js
@@ -1,5 +1,7 @@
 var bleno = require('bleno');
 var fs = require('fs');
+var sys = require('sys');
+var child_process = require('child_process');
 var Descriptor = bleno.Descriptor;
 var descriptor = new Descriptor({
      uuid: '2901',
@@ -7,6 +9,9 @@ var descriptor = new Descriptor({
 });
 
 var Characteristic = bleno.Characteristic;
+var myrun;
+var cmdstr;
+var running=0;
 var characteristic = new Characteristic({
     uuid:  '01010101010101010101010101010111',
     properties: ['write', 'writeWithoutResponse','notify'],
@@ -16,17 +21,37 @@ var characteristic = new Characteristic({
     console.log('************************');
 	console.log('We got: ' + data); // THIS BIT HERE is where we get data
 	var str = data.toString('utf8');
-	var args = str.split(":");	 
-	var speed = parseInt(args[0]);
-	var time = parseInt(args[1]);
-	 console.log('speed is: ' + speed);
-	console.log('time is: ' +time);
-	fs.writeFile("command.txt",str);
+	var args = str.split(":");
+	var keywrd = args[0];
+	var speed = parseInt(args[1]);
+	var time = parseInt(args[2]);
+	if(args[0]=="run" && running==0){
+	    console.log('speed is: ' + speed);
+	    console.log('turning time is: ' + time);
+//	    console.log(cmdstr);
+	     //   myrun = exec(cmdstr);
+	    //myrun = child_process.spawn('java',['-classpath','./SmartCar:classes:/opt/pi4j/lib/'*'','1000','1000']);
+	    myrun = child_process.spawn('./run',[speed, time],{detached: true});
+	    console.log('process is: ' +myrun.pid);
+	    running=1;
+	}
+	else if(args[0]=="stop" && running==1){
+	    console.log('killing process: ' + myrun.pid);
+	    process.kill(-myrun.pid); // first kill the original process
+	    // next stop the car
+	   
+	    myrun = child_process.spawn('./run',['0', '0'],{detached: true});
+	    console.log('stopping car with process:' + myrun.pid);
+	    running =0;
+	    // now kill the stop process
+//	    process.kill(-myrun.pid);
+	}
+//	fs.writeFile("command.txt",str); 
 //	fs.close();
      
    callback(Characteristic.RESULT_SUCCESS);
     }
-       });
+   });
 var PrimaryService = bleno.PrimaryService;
 var primaryService = new PrimaryService({
    //   uuid: 'fffffffffffffffffffffffffffffff0',
